7e190ddd9a2783fd3f259f84e96ace62819fe7b4
lizhiyuan <lizhiyuan2023@gmail.com>
2024-02-26 00:47:29 +0800

    接下来补充tailwindCss的配置文件了

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    // 以下直接覆盖默认的配置
+    color: {
+      // https://uicolors.app/create
+    },
+    // fontSize
+    fontSize: {},
+    extend: {}, // 这个应该是在默认配置的基础上进行扩展的
   },
   plugins: [],
 }
